36969f49dbb5239d642755a1cec2ebbb3b19334f
eduVieiraAF <edu_vieira@hotmail.com>
2021-10-31 08:38:17 -0300

    Taking shape pt. Deux

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -13,13 +13,31 @@ public class TicTacToe implements ActionListener {
     JLabel  textField = new JLabel();
     JButton[] buttons = new JButton[9];
     boolean player1_trun;
+    ImageIcon icon = new ImageIcon("tictactoe.png");
 
     TicTacToe(){
         board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         board.setSize(800,800);
         board.getContentPane().setBackground(new Color(45,45,45));
         board.setLayout(new BorderLayout());
+        board.setTitle("TIC TAC TOE");
+        board.setResizable(false);
+        board.setLocationRelativeTo(null);
+        board.setIconImage(icon.getImage());
         board.setVisible(true);
+
+        textField.setBackground(new Color(45,45,45));
+        textField.setForeground(new Color(0,158,158));
+        textField.setFont(new Font("Ink Free", Font.BOLD,55));
+        textField.setHorizontalAlignment(JLabel.CENTER);
+        textField.setText("TIC TAC TOE");
+        textField.setOpaque(true);
+
+        tittle.setLayout(new BorderLayout());
+        tittle.setBounds(0,0,800,10);
+
+        tittle.add(textField);
+        board.add(tittle);
 
     }
 
